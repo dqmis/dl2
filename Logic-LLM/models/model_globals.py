00f2e0b7400f8cbdc28805c546fed8574705b95c
jjhl <jjhlgraber@gmail.com>
2024-05-11 18:12:45 +0200

    cleaner global variable usage

--- Logic-LLM/models/model_globals.py
+++ Logic-LLM/models/model_globals.py
@@ -1,3 +1,4 @@
+# For info on the genimi names see https://cloud.google.com/vertex-ai/generative-ai/docs/learn/model-versioning#gemini-model-versions
 GENIMI_MODEL_NAMES = ["gemini-1.0-pro-vision-001", "gemini-1.5-pro-preview-0409"]
 GENIMI_PROJECT_ID = "logic-lm"
 GENIMI_LOCATION = "europe-west4"
--- Logic-LLM/models/utils.py
+++ Logic-LLM/models/utils.py
@@ -158,7 +158,6 @@ class OpenAIModel:
 
 
 import model_globals
-
 import vertexai
 from vertexai.generative_models import GenerativeModel, GenerationConfig
 
