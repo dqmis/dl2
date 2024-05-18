add6917acf062a7455387087a40f8e4ac22db921
ChimeneBlokesch <chimene.blokesch2002@gmail.com>
2024-05-18 16:21:04 +0200

    SET: loading images and texts

--- src/data/SET_wrapper.py
+++ src/data/SET_wrapper.py
@@ -1,6 +1,9 @@
 import os
+from PIL import Image
 
 from dataset_wrapper import DatasetWrapper
+
+from LLM_PuzzleTest.AlgoPuzzleVQA.data_loading import convert_image_to_text
 
 class GameSET(DatasetWrapper):
     def __init__(self):
@@ -12,11 +15,11 @@ class GameSET(DatasetWrapper):
         self.text_folder = "text"
         self.asp_folder = "asp"
 
-        self.image_data = self.load_files(self.images_folder)
-        self.text_data = self.load_files(self.text_folder)
-        self.asp_data = self.load_files(self.asp_folder)
+        self.image_files = self.find_files(self.images_folder)
+        self.text_files = self.find_files(self.text_folder)
+        self.asp_files = self.find_files(self.asp_folder)
 
-    def load_files(self, subfolder):
+    def find_files(self, subfolder):
         files = []
         for file in os.listdir(f"{self.root_folder}/{self.root_data_dir}/{subfolder}"):
             file = f"{self.root_folder}/{self.root_data_dir}/{self.images_folder}/{file}"
@@ -24,6 +27,27 @@ class GameSET(DatasetWrapper):
 
         return files
 
+    def load_data(self, file, is_image=False):
+        with open(file, mode='r', encoding='utf-8') as f:
+            data = f.readlines()
+
+        if is_image:
+            data = Image.open(data)
+            data = convert_image_to_text(data)
+
+        return data
+
 
     def evaluate(self, use_images=False):
-        prompt = "Analyze the provided image of a SET game layout. Identify all sets, where a set consists of three cards with each attribute (shape, color, number, shading) either all the same or all different. List each set by describing the attributes of the three cards. Response Example: 'Set 1: [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]' Set 2: ... If no sets are found, state 'No sets found'."
+        if use_images:
+            prompt = "Analyze the provided image of a SET game layout. Identify all sets, where a set consists of three cards with each attribute (shape, color, number, shading) either all the same or all different. List each set by describing the attributes of the three cards. Response Example: 'Set 1: [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]' Set 2: ... If no sets are found, state 'No sets found'."
+
+            for i in range(len(self.image_files)):
+                image_data = self.load_data(self.image_files[i], is_image=True)
+                yield prompt, image_data, None
+
+        prompt = "Analyze the provided text of a SET game layout. Identify all sets, where a set consists of three cards with each attribute (shape, color, number, shading) either all the same or all different. List each set by describing the attributes of the three cards. Response Example: 'Set 1: [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]; [Number], [Color], [Shading], [Shape]' Set 2: ... If no sets are found, state 'No sets found'."
+
+        for i in range(len(self.text_files)):
+            text = self.load_data(self.text_files[i])
+            yield f"{prompt} {text}", None, None
