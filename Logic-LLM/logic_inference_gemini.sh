7a8a7bdd3bc4edbe7bcfd55feff93a7b7baa9bcb
jjhl <jjhlgraber@gmail.com>
2024-05-29 12:27:02 +0200

    prepare last inference gemini

--- Logic-LLM/logic_inference_gemini.sh
+++ Logic-LLM/logic_inference_gemini.sh
@@ -11,16 +11,21 @@ model_names=("gemini-1.0-pro-vision-001" "gemini-1.5-pro-preview-0409" "gemini-1
 # gemini_model="gemini-1.5-pro-preview-0514"
 # gemini_model="gemini-1.5-flash-preview-0514"
 
-for gemini_model in "${model_names[@]}"
-    do
-        for task in "${tasks[@]}"
-            do
-                echo "Running program with task: $task"
-                python3 ./models/logic_inference.py \
-                    --model_name "$gemini_model" \
-                    --dataset_name "$task" \
-                    --split dev \
-                    --backup_strategy LLM \
-                    --backup_LLM_result_path "./baselines/results/CoT_${task}_dev_gemini-1.5-flash-preview-0514.json"
-            done
-    done
+# random already done
+backup_strategies=("Direct" "CoT")
+
+
+for backup in "${backup_strategies[@]}"
+    for gemini_model in "${model_names[@]}"
+        do
+            for task in "${tasks[@]}"
+                do
+                    echo "Running program with task: $task"
+                    python3 ./models/logic_inference.py \
+                        --model_name "$gemini_model" \
+                        --dataset_name "$task" \
+                        --split dev \
+                        --backup_strategy "$backup" \
+                        --backup_LLM_result_path "./baselines/results/${backup}_${task}_dev_${gemini_model}.json"
+                done
+        done
--- Logic-LLM/models/evaluation_save.py
+++ Logic-LLM/models/evaluation_save.py
@@ -6,34 +6,36 @@ import model_globals
 def full_evaluation_save():
     results_json = {}
 
-    backup = "LLM"
     split = model_globals.SPLIT_USED_IN_PAPER
+    backup_strategies = ["random", "Direct", "CoT"]
+
     # now only saving Gemini results
     for model_name in model_globals.GEMINI_MODEL_NAMES:
         results_json[model_name] = {}
         for dataset_name in model_globals.DATASET_NAMES:
-            result_path = os.path.join("outputs", "logic_inference")
-            result_file = os.path.join(result_path, f'{dataset_name}_{split}_{model_name}_backup-{backup}.json')
-            print(result_file)
-            if os.path.isfile(result_file):
-                with open(result_file, 'r') as f:
-                    all_samples = json.load(f)
-                executable_samples = [sample for sample in all_samples if sample['flag'] == 'success']
-                
-                overall_accuracy = evaluate_QA(all_samples)
-                executable_rate = len(executable_samples) / len(all_samples) if all_samples else 0
-                executable_accuracy = evaluate_QA(executable_samples)
-                
-                results_json[model_name][dataset_name] = {
-                    "Overall_Accuracy": overall_accuracy,
-                    "Executable_Rate": executable_rate,
-                    "Executable_Accuracy": executable_accuracy,
-                }
-            else:
-                print(f"No results available for {model_name} on {dataset_name}")
+            results_json[dataset_name] = {}
+            for backup in backup_strategies:
+                result_path = os.path.join("outputs", "logic_inference")
+                result_file = os.path.join(result_path, f'{dataset_name}_{split}_{model_name}_backup-{backup}.json')
+                if os.path.isfile(result_file):
+                    with open(result_file, 'r') as f:
+                        all_samples = json.load(f)
+                    executable_samples = [sample for sample in all_samples if sample['flag'] == 'success']
+                    
+                    overall_accuracy = evaluate_QA(all_samples)
+                    executable_rate = len(executable_samples) / len(all_samples) if all_samples else 0
+                    executable_accuracy = evaluate_QA(executable_samples)
+                    
+                    results_json[model_name][dataset_name][backup] = {
+                        "Overall_Accuracy": overall_accuracy,
+                        "Executable_Rate": executable_rate,
+                        "Executable_Accuracy": executable_accuracy,
+                    }
+                else:
+                    print(f"No results available for {model_name} on {dataset_name}")
     
     outputs_path = f'outputs'
-    eval_path = os.path.join(outputs_path, "evaluation", f'evaluation_logic_programs_{backup}.json')
+    eval_path = os.path.join(outputs_path, "evaluation", f'evaluation_logic_programs_gemini.json')
     with open(eval_path, 'w') as file:
         json.dump(results_json, file, indent=4)
 
