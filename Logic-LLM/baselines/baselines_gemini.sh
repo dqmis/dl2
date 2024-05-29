e658cd51a8425c8486a03618883e1c8a4b66eeab
jjhl <jjhlgraber@gmail.com>
2024-05-29 11:46:20 +0200

    more baseline Gemini

--- Logic-LLM/baselines/baselines_gemini.sh
+++ Logic-LLM/baselines/baselines_gemini.sh
@@ -4,13 +4,67 @@ eval "$(conda shell.bash hook)"
 conda deactivate
 conda activate DL2
 
-tasks=("ProntoQA" "ProofWriter" "FOLIO" "LogicalDeduction" "AR-LSAT")
 
 # gemini_model="gemini-1.0-pro-vision-001"
 # gemini_model="gemini-1.5-pro-preview-0409"
-gemini_model="gemini-1.5-pro-preview-0514"
+# gemini_model="gemini-1.5-pro-preview-0514"
 # gemini_model="gemini-1.5-flash-preview-0514"
 
+
+# for task in "${tasks[@]}"
+#     do
+#         echo "Running program in direct mode with task: $task"
+#         python3 gemini_baseline.py \
+#             --model_name "$gemini_model" \
+#             --dataset_name "$task" \
+#             --split dev \
+#             --mode Direct \
+#             --max_new_tokens 16
+#     done
+
+
+# for task in "${tasks[@]}"
+#     do
+#         echo "Running program in CoT mode with task: $task"
+#         python3 gemini_baseline.py \
+#             --model_name "$gemini_model" \
+#             --dataset_name "$task" \
+#             --split dev \
+#             --mode CoT \
+#             --max_new_tokens 1024
+#     done
+
+tasks=("AR-LSAT")
+
+gemini_model="gemini-1.5-pro-preview-0409"
+
+# for task in "${tasks[@]}"
+#     do
+#         echo "Running program in direct mode with task: $task"
+#         python3 gemini_baseline.py \
+#             --model_name "$gemini_model" \
+#             --dataset_name "$task" \
+#             --split dev \
+#             --mode Direct \
+#             --max_new_tokens 16
+#     done
+
+
+for task in "${tasks[@]}"
+    do
+        echo "Running program in CoT mode with task: $task"
+        python3 gemini_baseline.py \
+            --model_name "$gemini_model" \
+            --dataset_name "$task" \
+            --split dev \
+            --mode CoT \
+            --max_new_tokens 1024
+    done
+
+tasks=("ProntoQA" "ProofWriter" "FOLIO" "LogicalDeduction" "AR-LSAT")
+
+
+gemini_model="gemini-1.0-pro-vision-001"
 
 for task in "${tasks[@]}"
     do
@@ -22,6 +76,7 @@ for task in "${tasks[@]}"
             --mode Direct \
             --max_new_tokens 16
     done
+
 
 for task in "${tasks[@]}"
     do
